236c392a46518ebdf27148c9d798ed32a5951fa1
mfares07 <mfares07@gmail.com>
2023-05-15 01:06:53 -0300

    api.js

---
+++ js/api.js
@@ -0,0 +1,21 @@
+const nombre = document.getElementById('name');
+const apellido = document.getElementById('lastname');
+const mail = document.getElementById('email');
+const telefono = document.getElementById('phone');
+
+const apiPlaceholders = async (nat) => {
+    try {
+        const connect = await fetch(`https://randomuser.me/api/?nat=${nat}`)
+        const data = await connect.json()
+
+        nombre.placeholder = data.results[0].name.first
+        apellido.placeholder = data.results[0].name.last
+        mail.placeholder = data.results[0].email
+        telefono.placeholder = data.results[0].phone
+    }
+
+    catch (error) {
+        console.log(error)
+    }
+}
+apiPlaceholders('es')
