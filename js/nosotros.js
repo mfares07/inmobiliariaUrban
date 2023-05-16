57e9473b7df99f29cbecfc47ac6fda9789245cd4
EMaldonadoMay <maldoeze@gmail.com>
2023-05-16 01:28:37 -0300

    modificado el js consumo API

--- js/nosotros.js
+++ js/nosotros.js
@@ -1,20 +1,24 @@
 /* consumir una API para obtener imagenes random de Bs As */
 
-urlImagen = "";
-autorImagen = "Autor: ";
-imagenes = {};
-articuloSomos = document.getElementById("imagenRandom")
-fetch("https://pixabay.com/api/?key=36302400-6143fac907803f294da8caefe&q=buenos+aires+arquitectura&lang=es&image_type=photo")
-.then(imagenes => imagenes.json())
-.then(data => {
-    console.log(data.totalHits);
-    indice = Math.round(Math.random() * data.totalHits);
-    imagenes = data.hits;    
-    urlImagen = imagenes[indice].webformatURL;
-    autorImagen = autorImagen + imagenes[indice].user;
-    console.log(autorImagen);
-    console.log(urlImagen);
 
-    articuloSomos.innerHTML = `<img src="${urlImagen}" alt="${autorImagen}">`;
-})
-.catch(err => console.log(err))
+const apiKey = "36302400-6143fac907803f294da8caefe";
+const query = "buenos+aires+arquitectura";
+const url = `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(query)}&lang=es&image_type=photo&per_page=200`;
+console.log(url);
+
+fetch(url)
+    .then(respuesta => respuesta.json())
+    .then(data => {
+        console.log(data.totalHits);
+        indice = Math.round(Math.random() * data.totalHits);
+        urlImagen = data.hits[indice].webformatURL;
+        autorImagen = "Autor: " + data.hits[indice].user;
+
+        articuloSomos = document.getElementById("imagenRandom");
+        articuloSomos.innerHTML = `<img src="${urlImagen}" alt="${autorImagen}">`;
+
+        console.log(autorImagen);
+        console.log(urlImagen);
+
+    })
+    .catch(error => console.log(error))
