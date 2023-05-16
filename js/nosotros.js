666bae691fe30892b32f02acd365c26d307e2ca9
EMaldonadoMay <maldoeze@gmail.com>
2023-05-16 01:48:19 -0300

    cambio en el JS

--- js/nosotros.js
+++ js/nosotros.js
@@ -1,9 +1,9 @@
 /* consumir una API para obtener imagenes random de Bs As */
 
-
 const apiKey = "36302400-6143fac907803f294da8caefe";
 const query = "buenos+aires+arquitectura";
 const url = `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(query)}&lang=es&image_type=photo&per_page=200`;
+const articuloSomos = document.getElementById("imagenRandom");
 
 fetch(url)
     .then(respuesta => respuesta.json())
@@ -12,7 +12,6 @@ fetch(url)
         urlImagen = data.hits[indice].webformatURL;
         autorImagen = "Autor: " + data.hits[indice].user;
 
-        articuloSomos = document.getElementById("imagenRandom");
         articuloSomos.innerHTML = `<img src="${urlImagen}" alt="${autorImagen}">`;
     })
     .catch(error => console.log(error))
